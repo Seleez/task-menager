9185d925d38d273ac09d2914bb8d750a2fdb4e9c
Seleez <bartekczernek10@gmail.com>
2025-03-10 09:36:47 +0100

    Added routingfor home and tasks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { TaskDetailComponent } from './pages/task-detail/task-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'tasks', component: TasksComponent},
+    {path: 'tasks/:id', component: TaskDetailComponent}
+];
